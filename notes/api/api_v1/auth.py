a0f0359f1688247821583dd3415a2c44d18cf81b
ZHNEC77 <vadim030714@gmail.com>
2024-08-28 03:00:27 +0300

    fifth commit

--- notes/api/api_v1/auth.py
+++ notes/api/api_v1/auth.py
@@ -20,6 +20,19 @@ router.include_router(
     router=fastapi_users.get_auth_router(authentication_backend),
 )
 
+# /register
 router.include_router(
     router=fastapi_users.get_register_router(UserRead, UserCreate)
+)
+
+# /request-verify-token
+# /verify
+router.include_router(
+    router=fastapi_users.get_verify_router(UserRead),
+)
+
+# /forgot-password
+# /reset-password
+router.include_router(
+    router=fastapi_users.get_reset_password_router(),
 )
--- notes/api/api_v1/fastapi_users.py
+++ notes/api/api_v1/fastapi_users.py
@@ -8,5 +8,8 @@ from api.dependencies.authentication.backend import authentication_backend
 
 fastapi_users = FastAPIUsers[User, UserIdType](
     get_user_manager,
-    authentication_backend,
+    [authentication_backend],
 )
+
+current_user = fastapi_users.current_user(active=True)
+current_super_user = fastapi_users.current_user(active=True, superuser=True)
